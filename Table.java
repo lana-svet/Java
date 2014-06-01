913e4a14dc0438f2f7f967c32b08190948edb2cc
lana-svet <svetlana.volskaya@gmail.com>
2014-06-01 23:54:36 +0600

    fixed table

--- Table.java
+++ Table.java
@@ -1,50 +1,53 @@
-public class Table {
+public class Table2 {
 
     public static void main(String[] args) {
-        String[] firstLine = {"\u250C", "\u2500", "\u252C", "\u2500", "\u2510"};
-        String[] secondLine = {"\u251C", "\u2500", "\u253C", "\u2500", "\u2524"};
-        String[] thirdLine = {"\u2514", "\u2500", "\u2534", "\u2500", "\u2518"};
-        String[][] allTheLines = {firstLine, secondLine, thirdLine};
-        String between = "\u2502";
-        String table = "";
-        for (int i = 1; i < allTheLines.length + 1; i++) {
-            System.out.println(rows(firstLine, secondLine, thirdLine, table, i));
-            if (i == 1 | i == 2)
-                System.out.println(bordersOfCell(between));
-        }
+        int m = 5;
+        int n = 3;
+
+        String[] body1 = {"\u252C", "\u2500"};
+        String[] body2 = {"\u253C", "\u2500"};
+        String[] body3 = {"\u2534", "\u2500"};
+        String[] heads = {"\u250C", "\u251C", "\u2514"};
+        String[] tails = {"\u2510", "\u2524", "\u2518"};
+
+        System.out.println(makeLine(heads[0], m, body1, tails[0]));
+        linesRepeat(n, m, "\u2502", tails, heads, body2);
+        System.out.println(betweenString("\u2502", m));
+        System.out.println(makeLine(heads[2], m, body3, tails[2]));
     }
 
-    public static String rows(String[]firstLine, String[]secondLine, String[]thirdLine, String table, int i) {
-        if (i == 1) {
-            for (int t = 0; t < firstLine.length; t++) {
-                table += firstLine[t];
+    public static String innerLine(int m, String[] oneMass) {
+        String someString = "";
+        for (int i = 1; i < m; i++) {
+            for (int t = 0; t < 2; t++) {
+                someString += oneMass[t];
             }
-            return table;
         }
-        if (i == 2) {
-            for (int t = 0; t < secondLine.length; t++) {
-                table += secondLine[t];
-            }
-            return table;
-        }
-        if (i == 3) {
-            for (int t = 0; t < thirdLine.length; t++) {
-                table += thirdLine[t];
-            }
-            return table;
-        }
-        return "";
+        return someString;
     }
 
+    public static String makeLine(String head, int m, String[] body, String tail){
+        String newStr = head;
+        newStr += "\u2500";
+        newStr += innerLine(m, body);
+        newStr += tail;
+        return newStr;
+    }
 
-    public static String bordersOfCell(String val) {
-        String innerBordersLines = "";
-        for (int i = 0; i < 3; i++) {
-            if (i == 0)
-                innerBordersLines += val;
-            else
-                innerBordersLines += "." + val;
+    public static String betweenString(String someStr, int m) {
+        String betwString = "";
+        for (int i = 0; i < m; i++) {
+            betwString += someStr;
+            betwString += " ";
         }
-        return innerBordersLines;
+        betwString += someStr;
+        return betwString;
+    }
+
+    public static void linesRepeat(int n, int m, String someStr, String[] tails, String[] heads, String[] body2) {
+        for (int i = 0; i < n - 1; i++) {
+            System.out.println(betweenString(someStr, m));
+            System.out.println(makeLine(heads[1], m, body2, tails[1]));
+        }
     }
 }
